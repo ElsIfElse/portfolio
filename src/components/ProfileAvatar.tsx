abc24686a0b0088f1be86d9ae1fdf7970abd2f46
Sapimizera <szabolcs.sapi@gmail.com>
2025-03-14 19:47:15 +0100

    image fix 02

--- src/components/ProfileAvatar.tsx
+++ src/components/ProfileAvatar.tsx
@@ -11,7 +11,7 @@ const ProfileAvatar = () => {
         onHoverEnd={()=>setIsHovered(false)}
         
         className="rounded-full border-4 border-slate-700 w-[100px] h-[100px] bg-[rgba(255,255,255,0.1)] overflow-hidden">
-            {isHovered ? <img className="scale-130 translate-y-1 -translate-x-1" src={`${import.meta.env.BASE_URL}smiling.JPG`} alt="" /> : <img className="scale-130" src={`${import.meta.env.BASE_URL}serious.JPG`} alt="" />}
+            {isHovered ? <img className="scale-130 translate-y-1 -translate-x-1" src="./smiling.JPG" alt="" /> : <img className="scale-130" src="./serious.JPG" alt="" />}
         </motion.div>
      );
 }
--- src/helperFunctions/usePrintFrontDevProjects.tsx
+++ src/helperFunctions/usePrintFrontDevProjects.tsx
@@ -17,28 +17,28 @@ const usePrintFrontDevProjects = () => {
     [
         {
             name:"Recipe App",
-            image:`${import.meta.env.BASE_URL}recipeApp.png`,
+            image:"./recipeApp.png",
             description:"Built a full-stack social media platform for cooking enthusiasts. Implemented user authentication and profile management. Created CRUD operations for recipe posting and interaction.Implemented live chat system with websockets.",
             stack:["React","Javascript","CSS","Vite","Typescript","Websockets"], 
             link:"https://recipe-app-nine-iota.vercel.app/",
         },
         {
             name:"Dine at Mine",
-            image:`${import.meta.env.BASE_URL}dineAtMine.png`,
+            image:"./dineAtMine.png",
             description:"Designed and implemented a responsive restaurant website. Created animated UI elements and interactive menu components",
             stack:["React","Typescript","TailwindCSS","Material UI"],
             link:"https://dineatmine.vercel.app/",
         },
         {
             name:"DnD Npc database",
-            image:`${import.meta.env.BASE_URL}dndSite.png`,
+            image:"./dndSite.png",
             description:"Developed a tool for Dungeon Masters to manage NPC characters. Implemented complete database functionality with filtering options.",
             stack:["React","Typescript","TailwindCSS","NextJs","Express","MongoDb"],
             link:"https://egylet.vercel.app/",
         },
         {
             name:"AI companionship",
-            image:`${import.meta.env.BASE_URL}aiCom.png`,
+            image:"./aiCom.png",
             description:"Created an AI chatbot with customizable personality settings. Implemented responsive conversation UI with message history ",
             stack:["React","Typescript","TailwindCSS","Huggingface","Express"],
             link:"https://next-ai-companionship.vercel.app/",
