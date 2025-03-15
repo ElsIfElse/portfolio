dc9d2fb7b8b941b641513d75d5418527f122104d
Sapimizera <szabolcs.sapi@gmail.com>
2025-03-15 14:53:17 +0100

    added correct CV and made it downloadable

--- src/sections/ProfileCardSection.tsx
+++ src/sections/ProfileCardSection.tsx
@@ -25,13 +25,13 @@ const ProfileCardSection = () => {
                         }}
                         className="ml-1 text-4xl -translate-y-2">👋</motion.h2>
                     </div>
-                    <div className="flex flex-row items-center gap-2">
+                    <div className="flex flex-row items-center gap-2"> 
                         <div className="w-[7px] h-[7px] bg-green-500 rounded-full"></div>
                         <p>Available for work</p>
                     </div>
                     <div className="flex flex-row gap-1 translate-y-1">
                         <a href="https://www.linkedin.com/in/szabolcs-s%C3%A1p-mizera-4670b7205/" className="profile-link flex flex-row justify-center items-center gap-1"><img className="w-[20px] h-[20px]" src="https://img.icons8.com/ios11/512/linkedin.png" alt="" /> LinkedIn</a>
-                        <a className="profile-link flex flex-row justify-center items-center gap-1" download={"/Szabolcs_Sapi-Mizera_CV.pdf"} href=""><img className="w-[20px] h-[20px]" src="https://www.iconpacks.net/icons/1/free-document-icon-901-thumb.png" alt="" /> CV</a>
+                        <a className="profile-link flex flex-row justify-center items-center gap-1" media="application/pdf" type="application/pdf" download={"szabolcs_sapi-mizera_cv.pdf"} href="./szabolcs_sapi-mizera_cv.pdf"><img className="w-[20px] h-[20px]" src="https://www.iconpacks.net/icons/1/free-document-icon-901-thumb.png" alt="" /> CV</a>
                     </div>
                 </div> 
             </div>
