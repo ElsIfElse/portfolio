1312719a776796d4515832f818ea6f295c81ab97
Sapimizera <szabolcs.sapi@gmail.com>
2025-06-14 15:12:06 +0200

    fix_02

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, HashRouter } from 'react-router-dom'
+import {Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainPage from './pages/MainPage'
 import ProjectPage from './pages/ProjectPage'
@@ -6,12 +6,10 @@ import ProjectPage from './pages/ProjectPage'
 function App() {  
 
   return (
-    <HashRouter>
       <Routes>
           <Route path="/portfolio/" element={<MainPage/>} />
           <Route path="/portfolio/project/:id" element={<ProjectPage/>} />
       </Routes>
-      </HashRouter>
   )
 }
 
